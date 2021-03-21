8e2ae05f8fc92cbc62828d7299eb08b1485b4471
Agustín V <joseagustinveneveg@gmail.com>
2021-03-21 13:30:30 -0600

    [UPDATE] Seleccion de metodo dinamico, y arreglo de restricciones

--- files/parte2_p3.py
+++ files/parte2_p3.py
@@ -5,6 +5,7 @@ import parte2_p2 as p2
 import ast
 
 root = Tk() 
+labels = []
 
 def window():
 
@@ -34,39 +35,54 @@ def window():
 
 def params(num):
 
+    global labels
+
+    if(len(labels) > 0):
+        for label in labels:
+            label.destroy()
+
     selectLbl = Label(root, text="Ingrese parametros:")
     selectLbl.config(fg="black", bg="gray", font=("Times New Roman",15)) 
     selectLbl.place(x=275, y=10)
+    labels.append(selectLbl)
     
     funcLbl = Label(root, text="Funcion f(x)")
     funcLbl.config(fg="black", bg="gray", font=("Times New Roman",12)) 
     funcLbl.place(x=275, y=50)
+    labels.append(funcLbl)
 
     func = tk.Entry(root)
     func.place(x=275, y=70)
+    labels.append(func)
 
     tolLbl = Label(root, text="Tolerancia")
     tolLbl.config(fg="black", bg="gray", font=("Times New Roman",12)) 
     tolLbl.place(x=275, y=100)
+    labels.append(tolLbl)
 
     tol = tk.Entry(root)
     tol.place(x=275, y=120)
+    labels.append(tol)
 
     iterMLbl = Label(root, text="Iteraciones maximas")
     iterMLbl.config(fg="black", bg="gray", font=("Times New Roman",12)) 
     iterMLbl.place(x=275, y=150)
+    labels.append(iterMLbl)
 
     iterM = tk.Entry(root)
     iterM.place(x=275, y=170)
+    labels.append(iterM)
 
     if(num == 1 or num == 2):
         
         rangeLbl = Label(root, text="Intervalo")
         rangeLbl.config(fg="black", bg="gray", font=("Times New Roman",12)) 
         rangeLbl.place(x=275, y=200)
+        labels.append(rangeLbl)
 
         rangeValue = tk.Entry(root)
         rangeValue.place(x=275, y=220)
+        labels.append(rangeValue)
         
         if(num == 1):
             nRaph = tk.Button(root, text="Calcular",  command= lambda: check(func.get(), rangeValue.get(), tol.get(), iterM.get(), 1))
@@ -78,9 +94,11 @@ def params(num):
         valueLbl = Label(root, text="Valor inicial")
         valueLbl.config(fg="black", bg="gray", font=("Times New Roman",12)) 
         valueLbl.place(x=275, y=200)
+        labels.append(valueLbl)
 
         value = tk.Entry(root)
         value.place(x=275, y=220)
+        labels.append(value)
 
         nRaph = tk.Button(root, text="Calcular",  command= lambda: check(func.get(),value.get(), tol.get(), iterM.get(), 3))
 
@@ -89,14 +107,17 @@ def params(num):
         valuesLbl = Label(root, text="Valores iniciales")
         valuesLbl.config(fg="black", bg="gray", font=("Times New Roman",12)) 
         valuesLbl.place(x=275, y=200)
+        labels.append(valuesLbl)
 
         value1 = tk.Entry(root)
         value1.config(width=5)
         value1.place(x=275, y=220)
+        labels.append(value1)
 
         value2 = tk.Entry(root)
         value2.config(width=5)
         value2.place(x=330, y=220)
+        labels.append(value2)
 
         nRaph = tk.Button(root, text="Calcular",  command= lambda: check(func.get(), [value1.get(), value2.get()], tol.get(), iterM.get(), 4))
     
@@ -106,16 +127,16 @@ def check(func, rango, tol, iterM, num):
 
     try:
         
-        if(num == 1 or 2):
+        if(num == 1 or  num == 2):
             rango = ast.literal_eval(rango)
         elif(num == 3):
-            rango = eval(value.get())
+            rango = eval(rango)
         elif(num == 4):
             rango = [eval(rango[0]), eval(rango[1])]
 
         tol = eval(tol)
 
-        iterM = int(iterM.get())
+        iterM = int(iterM)
 
         result(func, rango, tol, iterM, num)
         
